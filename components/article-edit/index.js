b2d655ed9cfb563719b484ced9cf15ffd1f13cb9
allen <761169793@qq.com>
2019-02-20 00:04:47 +0800

    保存至草稿

--- components/article-edit/index.js
+++ components/article-edit/index.js
@@ -1,58 +1,91 @@
 // components/article-edit/index.js
+import {} from '/config/getData.js'
 Component({
-    /**
-     * 组件的属性列表
-     */
-    properties: {
-        pictureImagesList: {
-            type: 'String',
-            value: '',
-            observer: '_pictureImagesListChange'
-        }
-    },
-
-    /**
-     * 组件的初始数据
-     */
-    data: {
-        tempImages: [], //临时图片地址
-        tempImagesPath: [], //临时图片路径
-    },
-
-    /**
-     * 组件的方法列表
-     */
-    methods: {
-		upper(){},
-		lower(){},
-		scroll(){},
-        _pictureImagesListChange(newVal, oldVal) {
-            let data = JSON.parse(newVal);
-            this.setData({
-                tempImages: data.tempImages,
-                tempImagesPath: data.tempImagesPath
-            });
-			
-        },
-		addNewPhoto(){
-			wx.chooseImage({
-				sizeType: ['original', 'compressed'],
-				sourceType: ['album', 'camera'],
-				success: (res) => {
-					this.data.tempImages.push(...res.tempFiles);
-					this.data.tempImagesPath.push(...res.tempFilePaths);
-					
-					this.setData({
-						tempImages: this.data.tempImages,
-						tempImagesPath: this.data.tempImagesPath
-					});
-					// getApp().globalData.localStorages.storage.Set('tempImagesArray', data).then((res)=>{
-					// 	console.log('save success');
-					// }).catch((error)=>{
-					// 	console.log('save fail');
-					// });
-				}
-			})
-		}
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    pictureImagesList: {
+      type: 'String',
+      value: '',
+      observer: '_pictureImagesListChange'
     }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    tempImages: [], //临时图片地址
+    tempImagesPath: [], //临时图片路径
+    articleContent: '',
+    date: ''
+  },
+  lifetimes:{
+    detached(){
+      
+    }
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    upper() {},
+    lower() {},
+    scroll() {},
+    _pictureImagesListChange(newVal, oldVal) {
+      let data = JSON.parse(newVal);
+      if (data) {
+        this.setData({
+          tempImages: data.tempImages,
+          tempImagesPath: data.tempImagesPath
+        });
+      }
+    },
+    addNewPhoto() {
+      wx.chooseImage({
+        sizeType: ['original', 'compressed'],
+        sourceType: ['album', 'camera'],
+        success: (res) => {
+          this.data.tempImages.push(...res.tempFiles);
+          this.data.tempImagesPath.push(...res.tempFilePaths);
+
+          this.setData({
+            tempImages: this.data.tempImages,
+            tempImagesPath: this.data.tempImagesPath
+          });
+        }
+      })
+    },
+    getTextareaContent(e) {
+      this.data.articleContent = e.detail.value;
+    },
+    saveToDraft() {
+      wx.showModal({
+        title: '提示',
+        content: '确认保存笔记至草稿箱吗？',
+        success:(res)=>{
+          if(res.confirm){
+            let drafData = [{
+              tempImages: this.data.tempImages,
+              tempImagesPath: this.data.tempImagesPath,
+              articleContent: this.data.articleContent,
+              date: new Date()
+            }];
+            getApp().globalData.localStorages.storage.Set('articleDraftBox', drafData).then((res) => {
+              wx.navigateBack({
+                delta: 2
+              })
+            }).catch((error) => {
+              console.log(error);
+            });
+          }
+        }
+      })
+    },
+    articlePush(){
+      
+    }
+  }
 })
